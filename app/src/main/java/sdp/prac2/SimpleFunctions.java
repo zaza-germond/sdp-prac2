4f3f79f6d3a6ce9fad6cc6e75c94afac597a7281
Siyabulela Gongqa <G20G9176@ict.ru.ac.za>
2024-05-21 22:02:50 +0200

    Method that rounds each number in list to nearest hundered

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -1,7 +1,29 @@
 package sdp.prac2;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+    //public SimpleFunctions() {}
+    
+    public List<Integer> Task6(List<Integer> numbers) {
+        List<Integer> roundedNumbers = new ArrayList<>();
+        
+        for (int num : numbers) {
+            int roundedNum = roundUpToNearestMultipleOf100(num);
+            roundedNumbers.add(roundedNum);
+        }
+        
+        return roundedNumbers;
+    }
+    
+    private int roundUpToNearestMultipleOf100(int num) {
+        if (num % 100 == 0) {
+            return num; // No rounding needed
+        } else {
+            return ((num / 100) + 1) * 100; // Round up to nearest multiple of 100
+        }
+    }
+
+
 }
